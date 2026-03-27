1397ac22948c36e79b7795b2ef9b1b6ebb471b6c
duchiru <ngvit31082007@gmail.com>
2026-03-27 18:43:09 +0700

    fix: move template definition to header file

--- src/game_helper.hpp
+++ src/game_helper.hpp
@@ -1,8 +1,12 @@
 #pragma once
 
+#include <chrono>
+#include <sstream>
 #include <string>
 #include <functional>
 #include <type_traits>
+
+#include "game_logger.hpp"
 
 // Game Helper
 /**
@@ -11,4 +15,38 @@
 
 template <typename Function>
 auto measureExecutionTime(const std::string &label, Function func, bool enabled)
-    -> std::invoke_result_t<Function>;
+        -> std::invoke_result_t<Function>
+{
+    using ReturnT = std::invoke_result_t<Function>;
+    auto start = std::chrono::high_resolution_clock::now();
+
+    if constexpr (std::is_void_v<ReturnT>)
+    {
+        func();
+        auto end = std::chrono::high_resolution_clock::now();
+
+        if (enabled)
+        {
+            std::chrono::duration<double> duration = end - start;
+            std::stringstream msg;
+            msg << "execution time of " << "[" << label << "()]" << " = " << duration.count() << "s";
+            GameLogger::log(msg.str(), GameLogger::Level::DEBUG);
+        }
+        return;
+    }
+    else
+    {
+        ReturnT result = func();
+        auto end = std::chrono::high_resolution_clock::now();
+
+        if (enabled)
+        {
+            std::chrono::duration<double> duration = end - start;
+            std::stringstream msg;
+            msg << "execution time of " << "[" << label << "()]" << " = " << duration.count() << "s";
+            GameLogger::log(msg.str(), GameLogger::Level::DEBUG);
+        }
+
+        return result;
+    }
+}
