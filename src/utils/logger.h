8f4c2229196be565f42b65e14603ba6415645387
duchiru <ngvit31082007@gmail.com>
2026-05-07 17:19:59 +0700

    chore: comment unused things

--- src/utils/logger.h
+++ src/utils/logger.h
@@ -73,7 +73,7 @@ inline std::string levelToString(Level level) {
  * NOTE:
  *   Có thể thay đổi giá trị này khi init (ví dụ verbose_flag)
  */
-inline static Level min_level = Level::INFO;
+// inline static Level min_level = Level::INFO;
 
 // ---------- Level Color (ANSI escape codes) ----------
 
@@ -118,10 +118,10 @@ inline std::string getColor(Level level) {
 inline static std::ofstream log_file;
 
 // bật/tắt ghi log ra file
-inline static bool write_to_file = false;
+// inline static bool write_to_file = false;
 
 // xác định có đang ở judge mode hay không (ảnh hưởng format/log behavior)
-inline static bool is_judge_mode = false;
+// inline static bool is_judge_mode = false;
 
 // ---------- Functions ----------
 
